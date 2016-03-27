e7289ae776698fbf05e54ebb3d9c1c70981306e0
Troy M. Connor <troy0820@users.noreply.github.com>
2016-03-27 19:12:19 -0400

    [package]Added mocha and eta-config v4

---
+++ tests/tests.js
@@ -0,0 +1,8 @@
+const scc = require('../lib/spotcrimecity');
+const assert = require('assert');
+
+describe('just a regular function', function() {
+  it('should pass', function() {
+    assert(true);
+  });
+});
