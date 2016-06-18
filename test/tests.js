ead5d5d0b90cc63cb53eda987cda368b91371e05
Troy Connor <troy0820@users.noreply.github.com>
2016-06-18 19:34:32 -0400

    [test]Renamed test, used code and lab vs mocha

---
+++ test/tests.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const Code = require('code'); // Hapi assertion library
+const Lab = require('lab');
+const lab = exports.lab = Lab.script();
+const scc = require('../lib/spotcrimecity');
+
+lab.experiment('Set up tests', function() {
+  lab.test('Test to see if module exported object', function(done) {
+    Code.expect(scc).to.be.a.object();
+    done();
+  });
+
+  lab.test('Check to see if object has function `getCrimesCity`', function(done) {
+    console.log('This is the function', scc.getCrimesCity);
+    Code.expect(scc.getCrimesCity).to.be.a.function();
+    done();
+  });
+});
--- tests/tests.js
+++
@@ -1,8 +0,0 @@
-const scc = require('../lib/spotcrimecity');
-const assert = require('assert');
-
-describe('just a regular function', function() {
-  it('should pass', function() {
-    assert(true);
-  });
-});
